c96e6c48ea49927620fe15f9e02a04c7770ba75c
Steff Kempink <mswf@live.nl>
2015-09-16 17:40:32 +0200

    + libs + cooks

--- main.lua
+++ main.lua
@@ -33,20 +33,42 @@ end
 
 
 --When logging 'nil' there's a fallback string that gets printed
-Log.steb()
-Log.bobn()
-Log.tinas()
-Log.gwebl()
-Log.waka()
-
-Log.steb("-")
-
-createEnum("TestEnum", "One", "Two", "Three")
-Log.steb(TestEnum)
-
-Log.steb("-")
-local test = TestEnum.One
-Log.steb(test)
-
-
+-- Log.steb()
+-- Log.bobn()
+-- Log.tinas()
+-- Log.gwebl()
+-- Log.waka()
+--
+-- Log.steb("-")
+--
+-- createEnum("TestEnum", "One", "Two", "Three")
+-- Log.steb(TestEnum)
+--
+-- Log.steb("-")
+-- local test = TestEnum.One
+-- Log.steb(test)
+--
+--
 local tween = Tween()
+--
+
+-- local Vector3 = require "base/math/vector3"
+-- Log.steb("Got the library")
+--
+-- local v1 = Vector3(3,3,3)
+--
+-- Log.steb(v1)
+
+local cooks = require "application/credits"
+
+print("CREDITS:")
+
+for i=1, #cooks do
+	print("")
+	print(cooks[i][1])
+	print(string.upper(cooks[i][2]))
+
+end
+
+
+-- Log.steb(ffi)
