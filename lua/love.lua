027fd85bca2969315392d7f77dd810281e4ad730
Steff Kempink <mswf@live.nl>
2015-10-31 17:41:21 +0100

    + camera zoom * moved initializing -> creating pentagon functionality inside the CairoTree

--- lua/love.lua
+++ lua/love.lua
@@ -6,15 +6,7 @@ function love.load()
 
 	GlobalTree = CairoTree(0,0)
 	GlobalTree:setSize(40)
-
-	for x=1,10 do
-		for y=1,10 do
-			local node = CairoPentagon(GlobalTree)
-			node:setPosition(x-5,y-5,1)
-			local node = CairoPentagon(GlobalTree)
-			node:setPosition(x-5,y-5,2)
-		end
-	end
+	GlobalTree:initialzeToDimensions(10,10)
 end
 
 function love.update(dt)
@@ -37,19 +29,34 @@ function love.update(dt)
 	if (INPUTS["d"]) then
 		CAMOFFSET.X =  CAMOFFSET.X - keyMagnitude
 	end
+
+	if (INPUTS["q"]) then
+		if (CAMOFFSET.ZOOM > 0.1) then
+			CAMOFFSET.ZOOM = CAMOFFSET.ZOOM - 0.05
+		end
+	end
+
+	if (INPUTS["e"]) then
+		if (CAMOFFSET.ZOOM < 2) then
+			CAMOFFSET.ZOOM = CAMOFFSET.ZOOM + 0.05
+		end
+	end
 end
 
 CAMOFFSET = {}
 CAMOFFSET.X = 0
 CAMOFFSET.Y = 0
 
+CAMOFFSET.ZOOM = 1
 
 function love.draw()
 	love.graphics.translate(CAMOFFSET.X, CAMOFFSET.Y)
+	love.graphics.scale(CAMOFFSET.ZOOM, CAMOFFSET.ZOOM)
+
 
 	love.graphics.setColor(255,255,255)
 
-  love.graphics.print("GwebSux", 400, 300)
+  love.graphics.print("GwebSwag", 400, 300)
 
 	GlobalTree:draw()
 
@@ -134,64 +141,69 @@ CairoTree = class(Tree, CairoTree, function(self, rootX, rootY)
 
 	self.size = 1
 
+	self._width = 0
+	self._height = 0
+
 	self._currentHovered = nil
 
 	self._grid = {}
 end)
 
+function CairoTree:initialzeToDimensions(width, height)
+	local cairoConstructor = CairoPentagon
+
+	self._grid = {}
+	for x=1, width do
+		self._grid[x] = {}
+		for y=1, height do
+			self._grid[x][y] = {}
+
+			local node = cairoConstructor(GlobalTree)
+			node:setPosition(x,y,1)
+			local node = cairoConstructor(GlobalTree)
+			node:setPosition(x,y,2)
+		end
+	end
+
+	self._width = width
+	self._height = height
+end
+
 function CairoTree:registerInput()
 	local mouseX, mouseY = love.mouse.getPosition()
 
-	mouseX = mouseX - CAMOFFSET.X
-	mouseY = mouseY - CAMOFFSET.Y
+	local worldX = (mouseX - CAMOFFSET.X) / CAMOFFSET.ZOOM
+	local worldY = (mouseY - CAMOFFSET.Y) / CAMOFFSET.ZOOM
 
+	local gridPosX = worldX/self.size*.5
+	local gridPosY = worldY/self.size*.5
 
-	mouseX = mouseX/self.size*.5
-	mouseY = mouseY/self.size*.5
+	local gridX = math.floor(gridPosX) +1
+	local gridY = math.floor(gridPosY) +1
 
-	local gridX = math.floor(mouseX) +1
-	local gridY = math.floor(mouseY) +1
+	if  (gridX < 1 or gridX > self._width
+		or gridY < 1 or gridY > self._height) then
+		self._currentHovered = nil
+		return
+	end
 
 	local isVertical = ((gridX + gridY) % 2 == 0)
 
 	local zOffset = 1
 	if (isVertical) then
-		if (math.floor(mouseX) < math.round(mouseX)) then
+		if (math.floor(gridPosX) < math.round(gridPosX)) then
 			zOffset = 2
 		end
 	else
-		if (math.floor(mouseY) < math.round(mouseY)) then
+		if (math.floor(gridPosY) < math.round(gridPosY)) then
 			zOffset = 2
 		end
 	end
 
-	local grid = self._grid
-	local newHovered = nil
-
-	if (self._grid[gridX]) then
-		if (self._grid[gridX][gridY]) then
-			if (self._grid[gridX][gridY][zOffset]) then
-				newHovered = self._grid[gridX][gridY][zOffset]
-			end
-		end
-	end
-
-	if (not newHovered) then
-		self._currentHovered = nil
-	else
-		self._currentHovered = newHovered
-	end
+	self._currentHovered = self._grid[gridX][gridY][zOffset]
 end
 
 function CairoTree:addToGrid(cairoPentagon,x,y,z)
-	if (not self._grid[x]) then
-		self._grid[x] = {}
-	end
-
-	if (not self._grid[x][y]) then
-		self._grid[x][y] = {}
-	end
-
 	self._grid[x][y][z] = cairoPentagon
 end
 
@@ -205,14 +217,14 @@ Node = class(Node, function(self, tree)
 	tree:addNode(self)
 
 	self._RANDCOLOR = {}
-	self._RANDCOLOR[1] = math.random()*255
-	self._RANDCOLOR[2] = math.random()*255
-	self._RANDCOLOR[3] = math.random()*255
+	self._RANDCOLOR[1] = math.random()*205 +50
+	self._RANDCOLOR[2] = math.random()*205 +50
+	self._RANDCOLOR[3] = math.random()*205 +50
 
 	self._RANDCOLORLINE = {}
-	self._RANDCOLORLINE[1] = math.random()*255
-	self._RANDCOLORLINE[2] = math.random()*255
-	self._RANDCOLORLINE[3] = math.random()*255
+	self._RANDCOLORLINE[1] = math.random()*205 +50
+	self._RANDCOLORLINE[2] = math.random()*205 +50
+	self._RANDCOLORLINE[3] = math.random()*205 +50
 end)
 
 function Node:setVertices(vertices)
@@ -257,6 +269,8 @@ end
 
 function Node:drawHovered()
 	if (self._vertices) then
+		love.graphics.setColor(unpack(self._RANDCOLORLINE))
+
 		love.graphics.polygon('fill', self._vertices)
 	end
 end
@@ -278,7 +292,7 @@ end)
 --|    B    |    /    |
 --|---------|----/----|
 
-DEBUGSKIPDRAW = true
+-- DEBUGSKIPDRAW = true
 
 function CairoPentagon:setPosition(gridX,gridY, gridZ)
 	self.gridX = gridX
@@ -294,13 +308,14 @@ function CairoPentagon:setPosition(gridX,gridY, gridZ)
 	local startX = (gridX-1) *2 *scale
 	local startY = (gridY-1) *2 *scale
 
+	-- local offset = 0
 	local offset = scale*((math.sqrt(3)-1)*0.5)
+	-- local offset = scale
 
 	if (isVertical) then
 		if (gridZ == 1) then
 			-- A
 			-- if DEBUGSKIPDRAW then return end
-
 			self:setVertices({
 				{startX +scale,					startY +scale*2		- offset},
 				{startX,								startY +scale*2},
@@ -324,7 +339,6 @@ function CairoPentagon:setPosition(gridX,gridY, gridZ)
 		if (gridZ == 1) then
 			-- C
 			-- if DEBUGSKIPDRAW then return end
-
 			self:setVertices({
 				{startX+ offset,				startY+scale},
 				{startX,								startY},
