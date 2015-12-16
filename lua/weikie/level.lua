df7e66743d9e69c20351062272816bedb9455fe9
Weikie <weikie_yeh@hotmail.com>
2015-12-16 17:12:40 +0100

    *Level initialized the floor array

--- lua/weikie/level.lua
+++ lua/weikie/level.lua
@@ -2,47 +2,50 @@
 --require "lua/LuaXML/test"
 require "lua/weikie/floor"
 
+TILE_WIDTH = 1
+TILE_LENGTH = 1
+
 Level = class(Level, function(self)
 	Log.waka("level init")
 
-	self.levelObjects = {}
+	self.floorTiles = {}
 	self.width = 20
 	self.height = 15
 	self:initLevelObjects()
 	self:_loadFloor()
 end)
 
-
--- local arr = {}
--- local arr2 = {}
-
 function Level:initLevelObjects()
 	local loopCount = 20
-	local arr = self.levelObjects
-
-	--global i/n?
+	local tiles = self.floorTiles
 	for i=1, self.width do
-		arr[i] = {}
+		tiles[i] = {}
 		for n=1, self.height do
-			arr[i][n] = 1
+			tiles[i][n] = Floor()
+			self:setFloorTile(i, n, 0)
 		end
 	end
 end
 
 function Level:printTable()
-	Log.waka(self.levelObjects[1])
+	Log.waka(self.floorTiles[1])
 end
 
 function Level:printTableValue(x, y)
-	local innerArray = self.levelObjects[x]
+	local innerArray = self.floorTiles[x]
 	local b = innerArray[y]
 	Log.waka(type(b))
 	Log.waka(b)
 	--Log.waka(arr2[x])
 end
 
-function Level:setTile(x, y, value)
-	self.levelObjects[x][y].value = value
+function Level:setFloorTile(x, y, value)
+	--should be texture
+	local tile = self.floorTiles[x][y]
+	--tile:setPosition ?
+	tile.setPosition(tile, x * TILE_WIDTH, 0, -(y * TILE_LENGTH))
+	--tile.value = value
+
 end
 
 function Level:_loadFloor()
