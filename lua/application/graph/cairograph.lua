3a81b3c9c46c1d69654757302e4fe3e47e7dd020
Steff Kempink <mswf@live.nl>
2016-01-27 09:58:04 +0100

    + graph correctly calculates if world coordinates fall inside of the node it returns, otherwise it will look through its direct neighbours

--- lua/application/graph/cairograph.lua
+++ lua/application/graph/cairograph.lua
@@ -129,6 +129,63 @@ function CairoGraph:getGridPositionFromNode(cairoPentagon)
 	return nil
 end
 
+
+--https://love2d.org/wiki/PointInConcavePolygon
+local function getQuad(axis_x,axis_y,vert_x,vert_y)
+ if vert_x < axis_x then
+	 if vert_y < axis_y then
+		 return 1
+	 else
+		 return 4
+	 end
+ else
+	 if vert_y < axis_y then
+		 return 2
+	 else
+		 return 3
+	 end
+ end
+end
+
+math_abs = math.abs
+
+local function pointInPolygon(pgon, tx, ty)
+	if (#pgon < 6) then
+		return false
+	end
+
+	local x1 = pgon[#pgon - 1]
+	local y1 = pgon[#pgon]
+	local cur_quad = getQuad(tx,ty,x1,y1)
+	local next_quad
+	local total = 0
+	local i
+
+	for i = 1,#pgon,2 do
+		local x2 = pgon[i]
+		local y2 = pgon[i+1]
+		next_quad = getQuad(tx,ty,x2,y2)
+		local diff = next_quad - cur_quad
+
+		if (diff == 2) or (diff == -2) then
+			if (x2 - (((y2 - ty) * (x1 - x2)) / (y1 - y2))) < tx then
+				diff = -diff
+			end
+		elseif diff == 3 then
+			diff = -1
+		elseif diff == -3 then
+			diff = 1
+		end
+
+		total = total + diff
+		cur_quad = next_quad
+		x1 = x2
+		y1 = y2
+	end
+
+	return (math_abs(total)==4)
+end
+
 function CairoGraph:getNodeByWorldCoord(worldX, worldY)
 	local gridPosX = worldX/ (self.size*2)
 	local gridPosY = worldY/ (self.size*2)
@@ -154,8 +211,19 @@ function CairoGraph:getNodeByWorldCoord(worldX, worldY)
 		end
 	end
 
-	-- return self:getNodeByGridPos(gridX, gridY, zOffset)
-	return self._grid[gridX][gridY][zOffset]
+	local immediateNode = self._grid[gridX][gridY][zOffset]
+
+	if (pointInPolygon(immediateNode._vertices, worldX, worldY)) then
+		return immediateNode
+	else
+		for i=1,#immediateNode.neighbours do
+			if (pointInPolygon(immediateNode.neighbours[i]._vertices, worldX, worldY)) then
+				return immediateNode.neighbours[i]
+			end
+		end
+		Log.warning("[CairoGraph] couldn't a point within any of the polygons")
+		return immediateNode
+	end
 end
 
 function CairoGraph:getNodeByGridPos(x,y,z)
